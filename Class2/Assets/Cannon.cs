e54acc4717c8dddda079ecdea457e01d4888d0a3
Miguel Angel Montoya <miguel.mzbi@gmail.com>
2017-08-21 17:18:42 -0500

    PlaneGame

---
+++ Class2/Assets/Cannon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    public float speed;
+    public GameObject original;
+
+	// Use this for initialization
+	void Start () {
+
+        speed = 1;
+        // Way to retrieve a reference
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float h = -Input.GetAxis("Horizontal");
+        float v = -Input.GetAxis("Vertical");
+        transform.Translate(speed * h * Time.deltaTime, 0, 0, Space.Self);
+        transform.Rotate(speed * v, 0, 0);
+
+        if(Input.GetKeyUp(KeyCode.Space)) {
+
+            // Quaternion
+            // 4 value vector
+            // Describe rotation in 3d space
+            GameObject clone = Instantiate(original, transform.position, transform.rotation) as GameObject;
+            Rigidbody rb2 = clone.GetComponent<Rigidbody>();
+        }
+	}
+}
---
+++ Class2/Assets/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private Rigidbody rb;
+    private static GameObject instance;
+
+    // Property
+    // Better accssor methods
+    public static GameObject Instance {
+        get {
+            return instance;
+        }
+    }
+
+    void Awake() {
+        instance = this.gameObject;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        // Retrieve reference
+        rb = GetComponent<Rigidbody>();
+        // Up, right and forward
+        // These vectors are always unit vector
+        rb.AddForce(transform.up*50, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
