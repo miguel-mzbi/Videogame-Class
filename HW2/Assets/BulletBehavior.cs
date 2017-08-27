d980ac494c2e889cd7ec5194f134e54e26363b69
Miguel Angel Montoya <miguel.mzbi@gmail.com>
2017-08-27 11:50:58 -0500

    Kinda HW2 not finished

---
+++ HW2/Assets/BulletBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour {
+
+    private Rigidbody rb;
+    private int brickCount;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.up * 13, ForceMode.Impulse);
+        brickCount = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnCollisionEnter(Collision c) {
+        if(c.transform.tag == "Brick" && brickCount <= 2) {
+            Destroy(c.gameObject);
+            brickCount++;
+        }
+        else {
+            print("Destroying");
+            Destroy(this.gameObject.transform.parent.gameObject);
+            Destroy(this.gameObject);
+
+        }
+        if (c.transform.tag == "Player") {
+            print("Destroying tank");
+            Destroy(c.gameObject);
+        }
+    }
+}
---
+++ HW2/Assets/TurretBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBehavior : MonoBehaviour {
+
+    private float rotationSpeed;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+        rotationSpeed = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v1 = -Input.GetAxis("VerticalP1");
+        float v2 = -Input.GetAxis("VerticalP2");
+
+        if(Input.GetAxis("VerticalP1") != 0 && this.transform.name == "CannonPivotBlue") {
+            this.transform.Rotate(0, 0, rotationSpeed * v1 * Time.deltaTime);
+            print("Cañon1");
+        }
+        if(Input.GetAxis("VerticalP2") != 0 && this.transform.name == "CannonPivotRed") {
+            this.transform.Rotate(0, 0, rotationSpeed * v2 * Time.deltaTime);
+            print("Cañon2");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && this.transform.name == "CannonPivotBlue") {
+            GameObject bulletB = Instantiate(bullet, transform.GetChild(0).position, transform.rotation) as GameObject;
+            Rigidbody RigidbodyBulletB = bulletB.GetComponent<Rigidbody>();
+        }
+        if(Input.GetKeyDown(KeyCode.RightControl) && this.transform.name == "CannonPivotRed") {
+            GameObject bulletA = Instantiate(bullet, transform.GetChild(0).position, transform.rotation) as GameObject;
+            Rigidbody RigidbodyBulletA = bulletA.GetComponent<Rigidbody>();
+        }
+    }
+}
