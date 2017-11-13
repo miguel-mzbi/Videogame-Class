7c4395ef6eaae34cfb5b3011fbfb69553debaa48
Miguel Angel Montoya <miguel.mzbi@gmail.com>
2017-11-13 15:45:04 -0600

    2D

---
+++ 2DAndGUI/Assets/ProfBehav.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfBehav : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        this.animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float v = Input.GetAxis("Vertical");
+        animator.SetFloat("vertical", v);
+
+        if(v != 0) {
+            this.transform.Translate(this.transform.up * Time.deltaTime * v);
+        }
+	}
+}
---
+++ 2DAndGUI/Assets/UIBehav.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIBehav : MonoBehaviour {
+
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+    // onClick()
+	public void ClickTest() {
+        text.text = "Button Was Pressed";
+    }
+
+    // onValueChanged(Single)
+    public void ValueChangeTest(float v) {
+        text.text = "Value Changed";
+    }
+}
