a2f8926192f27cbb9f8dbc2d82ffc383ac8d9982
Miguel Angel Montoya <miguel.mzbi@gmail.com>
2017-11-13 15:45:18 -0600

    Navigation

---
+++ Navigation/Assets/navtest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navtest : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = new Vector3(2.5f, 2.6f, 7.7f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray rayito = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(rayito, out hit)) {
+            agent.destination = hit.point;
+        }
+	}
+}
